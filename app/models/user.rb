3e7c9a91f47ca861e57eb1d0f2dd60b0fd1fb178
Kazuki Hoshiyama <kxxxkhkh@gmail.com>
2020-11-11 19:22:13 +0900

    テストコードとバリデートの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,            presence: true
-  validates :encrypted_password,  presence: true        
-  validates :last_name,           presence: true,  format: {with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name,          presence: true,  format: {with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name_kana ,     presence: true,  format: {with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana ,    presence: true,  format: {with: /\A[ァ-ヶー－]+\z/ }
-  validates :birth_date ,         presence: true
 
+  with_options presence: true do      
+      validates :nickname           
+      validates :encrypted_password        
+      validates :last_name,           format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+      validates :first_name,          format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+      validates :last_name_kana ,     format: {with: /\A[ァ-ヶー－]+\z/ }
+      validates :first_name_kana ,    format: {with: /\A[ァ-ヶー－]+\z/ }
+      validates :birth_date          
+  end
 end
