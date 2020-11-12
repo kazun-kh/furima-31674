8d27c8fc514d53f796a3a28b6ce343ba4f428199
Kazuki Hoshiyama <kxxxkhkh@gmail.com>
2020-11-12 18:18:23 +0900

    出品した商品の情報をテーブルに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,30 @@
 class ItemsController < ApplicationController
+
+  before_action :authenticate_user!
+
   def index
   end
   
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+    if @item.save
+      redirect_to action: :index
+    else
+      render action: :new
+    end
+
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :info, :price, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :image)
+  end
+
 end
