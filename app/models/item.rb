f1aba415c9a1481f5c56e80849a555a86abe0ddb
Kazuki Hoshiyama <kxxxkhkh@gmail.com>
2020-11-12 12:30:38 +0900

    ActiveHashを導入,各モデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,11 @@
 class Item < ApplicationRecord
+  
   belongs_to :user
-
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :sales_status
+  belongs_to_active_hash :shipping_fee_status
+  belongs_to_active_hash :prefecture
 end
