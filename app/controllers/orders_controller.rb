21dbe4d2295f14c8970d1480c706a65d47f6bd7a
Kazuki Hoshiyama <kxxxkhkh@gmail.com>
2020-11-15 19:28:09 +0900

    購入画面の商品の表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,22 @@
 class OrdersController < ApplicationController
   def index
+    # binding.pry
+    @item = Item.find(params[:item_id])
     @user_item = UserItem.new
   end
 
+ 
   def create
+    # binding.pry
+    @user_item = UserItem.new(item_params)   
+     if @user_item.valid?
+       @user_item.save
+       redirect_to action: :index
+     end
   end
+
+    private
+    def item_params
+     params.require(:user_item).permit(:postal_code, :prefecture, :nickname, :prefecture, :city, :house_number, :building_name, :phone_number)
+    end
 end
--- app/models/user_item.rb
+++ app/models/user_item.rb
@@ -8,12 +8,12 @@ with_options presence: true do
  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
  
  validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
- validates :house_number, format: { with: /\A[ぁ-んァ-ン一-龥]/, /\A[0-9]+\z/, message: "is invalid. Input full-width characters."}
+ validates :house_number, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
  validates :phone_number, format: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters."}
 
 end
 validates :building_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-validates :prefecture numericality: { other_than: 1, message: "can't be blank" }
+validates :prefecture, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
     # 各テーブルにデータを保存する処理を書く
